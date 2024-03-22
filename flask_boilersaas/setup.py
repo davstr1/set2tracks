3e55b98526fe341d7a64bbc88925609ff4c23e55
davstr1 <hi@davstr1.com>
2024-03-22 16:59:29 +0100

    boilerplate 0.2.0

--- flask_boilersaas/setup.py
+++ flask_boilersaas/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flask_boilersaas",
-    version="0.2",
+    version="0.2.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
