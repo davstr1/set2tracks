be1dc3ce6b979855369234ed363eda1a6df07263
davstr1 <hi@davstr1.com>
2024-03-24 10:22:12 +0100

    BUILD boilersaas 0.6.2

--- boilersaas/setup.py
+++ boilersaas/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="boilersaas",
-    version="0.6.1",
+    version="0.6.2",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     #include_package_data=True,
