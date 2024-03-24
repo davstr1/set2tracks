d8f053428fd5f456648d46fd130906624e7112e0
davstr1 <hi@davstr1.com>
2024-03-24 18:54:47 +0100

    REFAC move tailwind to project root

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./web/app/main/templates/*.html",
+    "./web/app/main/templates/**/*.html",
+    "./boilersaas/src/boilerssas/templates/*.html",
+    "./boilersaas/src/boilersaas/templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
