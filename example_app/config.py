2882af962197e17dc4ba569304c92a963f902240
davstr1 <hi@davstr1.com>
2024-03-24 10:46:26 +0100

    FEAT test required env variable are set before running

--- example_app/config.py
+++ example_app/config.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-import os
+import os,sys
 load_dotenv()
 
 SIGNUP_OPTIONS = {
@@ -9,23 +9,25 @@ SIGNUP_OPTIONS = {
 }
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-    SECRET_KEY = os.environ.get('SECRET_KEY') 
-    DB_TABLE_USERS = os.environ.get('DB_TABLE_USERS')
-    DB_TABLE_INVITES = os.environ.get('DB_TABLE_INVITES')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    PWD_SALT_LENGTH = int(os.environ.get('PWD_SALT_LENGTH'))
-    PWD_HASH_METHOD = os.environ.get('PWD_HASH_METHOD')
-    LANGUAGES = ['en', 'es','fr']
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
-    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
-    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
-    SIGNUP_OPTIONS = SIGNUP_OPTIONS
+    try:
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+        SECRET_KEY = os.environ['SECRET_KEY'] 
+        DB_TABLE_USERS = os.environ['DB_TABLE_USERS']
+        DB_TABLE_INVITES = os.environ['DB_TABLE_INVITES']
+        PWD_SALT_LENGTH = int(os.environ['PWD_SALT_LENGTH'])
+        PWD_HASH_METHOD = os.environ['PWD_HASH_METHOD']
+        LANGUAGES = ['en', 'es', 'fr']
+        MAIL_SERVER = os.environ['MAIL_SERVER']
+        MAIL_PORT = os.environ['MAIL_PORT']
+        MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
+        MAIL_USERNAME = os.environ['MAIL_USERNAME']
+        MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+        MAIL_DEFAULT_SENDER = os.environ['MAIL_DEFAULT_SENDER']
+        GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
+        GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
+        SIGNUP_OPTIONS = SIGNUP_OPTIONS
+    except KeyError as e:
+        sys.exit(f"Missing environment variable(s). Please add all the required environments variables as instructed in the README")
     
 
     
