a26c22e8d5bfe633513a1eeb756e95f0280d59d3
davstr1 <hi@davstr1.com>
2024-09-08 19:13:50 +0200

    FEAT complete channel and set visibility toggle routes

--- web/routes/admin_routes.py
+++ web/routes/admin_routes.py
@@ -1,10 +1,11 @@
+from operator import is_
 from flask import Blueprint, flash, jsonify, redirect, render_template, request, url_for
 from flask_login import current_user
 
-from web.controller import add_track_to_playlist, add_track_to_playlist_last_used, change_playlist_title, create_playlist, create_playlist_from_set_tracks, create_spotify_playlist_and_add_tracks, delete_playlist, get_playlist_with_tracks, get_playlists_from_user, remove_track_from_playlist, update_playlist_positions_after_track_change_position
-from web.lib.spotify import ensure_valid_token
-from web.routes.routes_utils import tpl_utils,get_user_id, is_connected, jax_redirect_if_not_connected
+
+from web.controller import channel_toggle_visibility, set_toggle_visibility
 from web.logger import logger
+from web.routes.routes_utils import is_admin
 
 
 
@@ -13,17 +14,35 @@ admin_bp = Blueprint('admin', __name__)
 
 @admin_bp.route('/admin/set/hide/<int:set_id>', methods=['GET'])  
 def set_visibility_toggle(set_id):
-    # admin check
-    # do the action
+    
+    if not is_admin():
+        flash('You are not an admin', 'error')
+        return redirect(url_for('set.sets'))
+   
+    result = set_toggle_visibility(set_id)
+    
+    if isinstance(result, dict) and 'error' in result:
+        flash(result['error'], 'error')
+    elif isinstance(result, dict) and 'message' in result:
+        flash(result['message'], 'success')
         
-    return render_template(url_for('set',set_id=set_id))
+    return redirect(url_for('set.sets'))
 
 
 @admin_bp.route('/admin/channel/hide/<int:channel_id>', methods=['GET'])  
 def channel_visibility_toggle(channel_id):
-    # admin check
-    # do the action
+
+    if not is_admin():
+        flash('You are not an admin', 'error')
+        return redirect(url_for('set.sets'))
     
-    return 1
+    result = channel_toggle_visibility(channel_id)
+    
+    if isinstance(result, dict) and 'error' in result:
+        flash(result['error'], 'error')
+    elif isinstance(result, dict) and 'message' in result:
+        flash(result['message'], 'success')
+    
+    return redirect(url_for('set.sets'))
         
     #return render_template(url_for('set',set_id=set_id))
