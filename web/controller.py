344542fdd6a17d4988d9dc2d301093c2e1aaf3f5
davstr1 <hi@davstr1.com>
2024-10-20 09:15:43 +0200

    FIX replace [0;31mERROR:0m [youtube] in discarded reason

--- web/controller.py
+++ web/controller.py
@@ -177,6 +177,7 @@ def clean_discarded_reason(reason, video_id=None):
     # Remove video_id from the reason. like in "n5l6paz89bg: this live event will begin in..."
     if video_id:
         reason = reason.replace(f"{video_id}:", "").strip()
+        reason = reason.replace(f"[0;31mERROR:[0m [youtube] ", "").strip()
     reason = reason.lstrip(":").strip()
     return cut_to_if_needed(reason, 255)
     
