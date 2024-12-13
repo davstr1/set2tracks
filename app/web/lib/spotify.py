89c0c8f86529a50800da3b6b497540789c1d9f7d
davstr1 <hi@davstr1.com>
2024-12-13 15:09:49 +0100

    FIX Spotify multiusers

--- app/web/lib/spotify.py
+++ app/web/lib/spotify.py
@@ -351,7 +351,7 @@ def get_client_token():
     code = request.args.get('code')
     if code:
         logger.info(f'Code: {code}')
-        token_info = sp_oauth.get_access_token(code)
+        token_info = sp_oauth.get_access_token(code,check_cache=False)
         session['token_info'] = token_info
         return token_info
     
