5b3f5c28832e0c5a2322d90550016bdcfa7dc7bc
davstr1 <hi@davstr1.com>
2024-12-19 14:11:27 +0100

    FEAT rewrite to make it a videoobject with clips

--- app/web/lib/json_schemas.py
+++ app/web/lib/json_schemas.py
@@ -1,67 +1,50 @@
 import json
+from turtle import up
 
 from web.lib.format import spotify_track_url
 
 
-def generate_set_json_schema(set_data, request_url):
+def generate_video_object_with_tracklist(set_data, request_url):
     """
-    Generate JSON object similar to the provided Jinja2 template.
+    Generate JSON-LD schema for a YouTube video with enhanced tracklist details.
 
     :param set_data: Dictionary containing set data
     :param request_url: URL of the current request
-    :param tpl_utils: Utility object with helper methods, e.g., for Spotify URLs
     :return: JSON object
     """
+   
+    
     json_object = {
         "@context": "https://schema.org",
-        "@type": "CreativeWork",
-        "name": set_data["title"],
-        "creator": {
-            "@type": "Person",
-            "name": set_data["channel"].author
-        },
+        "@type": "VideoObject",
+        "name": set_data["title"] + " with Tracklist",
         "description": (
-            f"A playlist created from '{set_data['title']}' by {set_data['channel'].author}. "
-            "Discover songs and export tracks to Spotify or Apple Music."
+            f"Watch '{set_data['title']}', navigate the video by tracks, preview songs, "
+            "and export them to Spotify or Apple Music."
         ),
-        "url": request_url,
-        "isBasedOn": {
-            "@type": "VideoObject",
-            "url": f"https://www.youtube.com/watch?v={set_data['video_id']}",
-            "name": set_data["title"],
-            "creator": {
-                "@type": "Person",
-                "name": set_data["channel"].author
-            },
-            "description": "The original mix from which this playlist was created.",
-            "thumbnailUrl": set_data["thumbnail"]
-        },
+        "thumbnailUrl": set_data["thumbnail"], # TODO : custom thumbnail by adding "with tracklist" text
+        "uploadDate": set_data['upload_date'],
+        "contentUrl": f"https://www.youtube.com/watch?v={set_data['video_id']}",
+        "embedUrl": f"https://www.youtube.com/embed/{set_data['video_id']}",
+        "duration": f"PT{set_data.get('duration', 3600)}S",
         "hasPart": []
     }
 
+    # Add tracks as parts of the video
     for track in set_data["tracks"]:
-        if track["id"] != 1:
-            track_data = {
-                "@type": "MusicRecording",
-                "name": track["title"],
-                "url": spotify_track_url(track),
-                "inPlaylist": set_data["title"],
-                "duration": f"PT{track['end_time'] - track['start_time']}S",
-                "byArtist": {
-                    "@type": "MusicGroup",
-                    "name": track["artist_name"]
-                }
+        track_data = {
+            "@type": "Clip",
+            "name": track["title"],
+            "startOffset": track["start_time"],
+            "endOffset": track["end_time"],
+            "url": f"{request_url}?t={track['start_time']}",  # Link directly to timestamp
+            "partOf": {
+                "@type": "VideoObject",
+                "name": set_data["title"],
+                "url": f"https://www.youtube.com/watch?v={set_data['video_id']}"
             }
-
-            if "cover_art" in track and track["cover_art"]:
-                track_data["image"] = track["cover_art"]
-
-            if "release_date" in track and track["release_date"]:
-                track_data["datePublished"] = track["release_date"]
-
-            if "genres" in track and track["genres"]:
-                track_data["genre"] = [genre.name for genre in track["genres"]]
-
-            json_object["hasPart"].append(track_data)
+        }
+        json_object["hasPart"].append(track_data)
 
     return json.dumps(json_object, indent=4)
+
