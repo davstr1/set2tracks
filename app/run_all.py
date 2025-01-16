c0d0dbf1bf1e9189ee1e42d1c73dc2453eca5502
davstr1 <hi@davstr1.com>
2025-01-16 08:51:02 +0100

    FIX rm inser set and app run from the processes. HAve them start separately for more clarity and control

--- app/run_all.py
+++ app/run_all.py
@@ -1,13 +1,13 @@
 import subprocess
 
 # Start the foreground process (blocking)
-foreground_process = subprocess.Popen("python run.py", shell=True)
+#foreground_process = subprocess.Popen("python run.py", shell=True)
 
 # Start background processes (non-blocking)
 background_scripts = [
     "python cron_check_channels.py",
     "python cron_remove_temp_downloads.py",
-    "python cron_set_insert.py",
+   # "python cron_set_insert.py",
     "python cron_set_queue.py"
 ]
 
@@ -15,12 +15,12 @@ background_processes = [
     subprocess.Popen(script, shell=True) for script in background_scripts
 ]
 
-try:
-    # Wait indefinitely for the foreground process
-    foreground_process.wait()
-except KeyboardInterrupt:
-    # Handle shutdown signal (e.g., Ctrl+C)
-    print("Shutting down...")
-    for process in background_processes:
-        process.terminate()
-    foreground_process.terminate()
+# try:
+#     # Wait indefinitely for the foreground process
+#     foreground_process.wait()
+# except KeyboardInterrupt:
+#     # Handle shutdown signal (e.g., Ctrl+C)
+#     print("Shutting down...")
+#     for process in background_processes:
+#         process.terminate()
+#     foreground_process.terminate()
