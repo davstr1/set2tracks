9488b1c80f72ff50962063f78bb87395d7485d08
davstr1 <hi@davstr1.com>
2025-02-01 21:55:44 +0100

    COPY change version

--- boilersaas/setup.py
+++ boilersaas/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="boilersaas",
-    version="0.7.0",
+    version="0.7.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
